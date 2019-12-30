5395aa48a492acc88b74ebd4b8222772acaad845
David <david@rowetel.com>
2019-12-31 08:46:08 +1030

    various NN and linear interpolation experiments

--- rateK_train.py
+++ rateK_train.py
@@ -9,8 +9,8 @@
 '''
   Usage:
 
-  $ c2sim ~/Downloads/train_8k.sw --rateK --rateKout ~/phasenn/rateK.f32
-  $ ./rateK_train.py rateK.f32 --dec 4 --frame 30 --epochs 25
+  $ ~/codec2/build_linux/src/c2sim ~/Downloads/all_speech_8k.sw --bands all_speech_8k.f32 
+  $ ./rateK_train.py all_speech_8k.f32 --dec 3
 '''
 
 import numpy as np
@@ -31,7 +31,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # constants
 
 nb_batch          = 32
-newamp1_K         = 20
+default_eband_K   = 14
 nb_plots          = 6
 N                 = 80
 
@@ -39,64 +39,86 @@ def list_str(values):
     return values.split(',')
 
 parser = argparse.ArgumentParser(description='Train a NN to interpolate rate K vectors')
-parser.add_argument('featurefile', help='f32 file of newamp1 rate K vectors')
-parser.add_argument('--dec', type=int, default=4, help='decimation rate')
+parser.add_argument('featurefile', help='f32 file of rate K vectors')
+parser.add_argument('--eband_K', type=int, default=default_eband_K, help='Length of eband vector')
+parser.add_argument('--dec', type=int, default=3, help='decimation rate')
 parser.add_argument('--frame', type=int, default="30", help='Frames to view')
 parser.add_argument('--epochs', type=int, default=10, help='Number of training epochs')
+parser.add_argument('--nn', action='store_true', help='Run interpolation NN')
+parser.add_argument('--nnpf', action='store_true', help='Run pf coeff NN')
 args = parser.parse_args()
 dec = args.dec
+eband_K = args.eband_K
 
 # read in rate K vectors
 features = np.fromfile(args.featurefile, dtype='float32')
-nb_features = newamp1_K
+nb_features = eband_K
 nb_samples = int(len(features)/nb_features)
 print("nb_samples: %d" % (nb_samples))
-rateK = np.reshape(features, (nb_samples, nb_features))/20
+rateK = np.reshape(features, (nb_samples, nb_features))
 print(rateK.shape)
 
 # set up training data
 nb_vecs = int(nb_samples/dec)
-inputs  = np.zeros((nb_vecs, 2*newamp1_K))
-outputs = np.zeros((nb_vecs, 3*newamp1_K))
-outputs_lin = np.zeros((nb_vecs, 3*newamp1_K))
-outputs_linpf = np.zeros((nb_vecs, 3*newamp1_K))
-nv = 0
-for i in range(0,nb_samples-dec,dec):
-    inputs[nv,:newamp1_K] = rateK[i,:]
-    inputs[nv,newamp1_K:] = rateK[i+dec,:]
-    for j in range(dec-1):
-        st = j*newamp1_K
-        outputs[nv,st:st+newamp1_K] = rateK[i+1+j,:]
+print("nb_samples: %d nb_vecs: %d dec: %d" % (nb_samples, nb_vecs, dec))
+inputs  = np.zeros((nb_vecs, 2*eband_K))
+outputs = np.zeros((nb_vecs, (dec-1)*eband_K))
+outputs_lin = np.zeros((nb_vecs, (dec-1)*eband_K))
+outputs_linpf = np.zeros((nb_vecs, (dec-1)*eband_K))
+output_c = np.zeros((nb_vecs, dec-1))
+outputs_step = np.zeros((nb_vecs, (dec-1)*eband_K))
+outputs_linstep = np.zeros((nb_vecs, (dec-1)*eband_K))
+
+for i in range(nb_vecs-dec):
+    j = i*dec
+    inputs[i,:eband_K] = rateK[j,:]
+    inputs[i,eband_K:] = rateK[j+dec,:]
+    # target outputs
+    for d in range(dec-1):
+        st = d*eband_K
+        outputs[i,st:st+eband_K] = rateK[j+d+1,:]
     # linear interpolation for reference
     c = 1.0/dec; inc = 1.0/dec;
-    for j in range(dec-1):
-        st = j*newamp1_K
-        outputs_lin[nv,st:st+newamp1_K] = (1-c)*inputs[nv,:newamp1_K] + c*inputs[nv,newamp1_K:]
+    for d in range(dec-1):
+        st = d*eband_K
+        outputs_lin[i,st:st+eband_K] = (1-c)*inputs[i,:eband_K] + c*inputs[i,eband_K:]
         c += inc
     # linear interpolation with per frame selection of c
-    for j in range(dec-1):
-        A = inputs[nv,:newamp1_K]; B = inputs[nv,newamp1_K:];
-        T = rateK[i+1+j,:]
+    A = inputs[i,:eband_K]; B = inputs[i,eband_K:];
+    for d in range(dec-1):
+        T = rateK[j+d+1,:]
         c = -np.dot((B-T),(A-B))/np.dot((A-B),(A-B))
-        st = j*newamp1_K
-        outputs_linpf[nv,st:st+newamp1_K] = c*A + (1-c)*B
-    
-    nv += 1
-print(inputs.shape, outputs.shape)
+        st = d*eband_K
+        outputs_linpf[i,st:st+eband_K] = c*A + (1-c)*B
+        output_c[i,d] = c
 
-nn = 1
-if nn:
+    # linear interpolation/step function (requires a bit)
+    for d in range(dec-1):
+        st = d*eband_K
+        outputs_step[i,st:st+eband_K] = inputs[i,eband_K:]
+    # determine linear interp error
+    e_lin = np.var(outputs[i,:] - outputs_lin[i,:])    
+    # determine step function error
+    e_step = np.var(outputs[i,:] - outputs_step[i,:])    
+    if e_lin < e_step:
+        outputs_linstep[i,:] = outputs_lin[i,:]
+    else:
+        outputs_linstep[i,:] = outputs_step[i,:]
+        
+if args.nn:
+    print(inputs.shape, outputs.shape)
+
     # our model
     model = models.Sequential()
-    model.add(layers.Dense(3*newamp1_K, activation='tanh', input_dim=2*newamp1_K))
-    model.add(layers.Dense(3*newamp1_K, activation='tanh'))
-    model.add(layers.Dense(3*newamp1_K))
+    model.add(layers.Dense(3*eband_K, activation='relu', input_dim=2*eband_K))
+    model.add(layers.Dense(3*eband_K, activation='relu'))
+    model.add(layers.Dense((dec-1)*eband_K))
     model.summary()
 
     # fit the model
     from keras import optimizers
-    #sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='mse', optimizer="adam")
+    sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='mse', optimizer=sgd)
     history = model.fit(inputs, outputs, batch_size=nb_batch, epochs=args.epochs, validation_split=0.1)
 
     # test the model on the training data
@@ -110,11 +132,55 @@ if nn:
     plt.xlabel('epoch')
     plt.show(block=False)
 
-# plot results over all frames
-var_lin = np.var(20*outputs-20*outputs_lin)
-var_linpf = np.var(20*outputs-20*outputs_linpf)
-var_nnest = np.var(20*outputs-20*outputs_nnest)
-print("var_lin: %3.2f var_linpf: %3.2f var_nnest: %3.2f" % (var_lin, var_linpf, var_nnest))
+# attempt to estimate pf coeffcients from vectors
+if args.nnpf:
+    print(inputs.shape, output_c.shape)
+    print(output_c[:10])
+    # our model
+    model = models.Sequential()
+    model.add(layers.Dense(3*eband_K, activation='relu', input_dim=2*eband_K))
+    model.add(layers.Dense(3*eband_K, activation='relu'))
+    model.add(layers.Dense(dec-1))
+    model.summary()
+
+    # fit the model
+    from keras import optimizers
+    sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='mse', optimizer=sgd)
+    history = model.fit(inputs, output_c, batch_size=nb_batch, epochs=args.epochs, validation_split=0.1)
+    
+    # test the model on the training data
+    c_est = model.predict(inputs)
+
+    plt.figure(1)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.legend(['train', 'valid'], loc='upper right')
+    plt.title('model loss')
+    plt.xlabel('epoch')
+    plt.show(block=False)
+
+# plot results over all frames, ebands are log10(energies) so multiply
+# by 10 to get variance in dB*dB
+var_lin = np.var(10*outputs-10*outputs_lin)
+var_linpf = np.var(10*outputs-10*outputs_linpf)
+var_linstep = np.var(10*outputs-10*outputs_linstep)
+if args.nn:
+    var_nnest = np.var(10*outputs-10*outputs_nnest)
+    print("var_lin: %3.2f var_linpf: %3.2f var_nnest: %3.2f" % (var_lin, var_linpf, var_nnest))
+elif args.nnpf:
+    # linear interpolation with per frame selection of c
+    outputs_linpf_est = np.zeros((nb_vecs, (dec-1)*eband_K))
+    for i in range(nb_vecs-dec):
+        A = inputs[i,:eband_K]; B = inputs[i,eband_K:];
+        for d in range(dec-1):
+            c = c_est[i,d]
+            st = d*eband_K
+            outputs_linpf_est[i,st:st+eband_K] = c*A + (1-c)*B
+    var_nnpfest = np.var(10*outputs-10*outputs_linpf_est)
+    print("var_lin: %3.2f var_linpf: %3.2f var_nnpfest: %3.2f" % (var_lin, var_linpf, var_nnpfest))
+else:
+    print("var_lin: %3.2f var_linpf: %3.2f var_linstep: %3.2f" % (var_lin, var_linpf, var_linstep))
 
 # plot results for a few frames
 
@@ -130,26 +196,33 @@ while loop:
     for d in range(dec+1):
         plt.subplot(1, nb_plots, d+1)
         if d == 0:
-            plt.plot(inputs[frame,:newamp1_K],'g')
+            plt.plot(inputs[frame,:eband_K],'g')
         elif d == dec:
-            plt.plot(inputs[frame,newamp1_K:],'g')
+            plt.plot(inputs[frame,eband_K:],'g')
         else: 
-            st = (d-1)*newamp1_K
-            plt.plot(outputs[frame,st:st+newamp1_K],'g')
-            plt.plot(outputs_lin[frame,st:st+newamp1_K],'b')
-            if nn:
-                plt.plot(outputs_nnest[frame,st:st+newamp1_K],'r')
+            st = (d-1)*eband_K
+            plt.plot(outputs[frame,st:st+eband_K],'g')
+            plt.plot(outputs_lin[frame,st:st+eband_K],'b')
+            plt.plot(outputs_linstep[frame,st:st+eband_K],'c')
+            if args.nn:
+                plt.plot(outputs_nnest[frame,st:st+eband_K],'r')
+            elif args.nnpf:
+                plt.plot(outputs_linpf_est[frame,st:st+eband_K],'r')
             else:
-                plt.plot(outputs_linpf[frame,st:st+newamp1_K],'r')
-        plt.ylim((-1,4))
-    var_lin = np.var(20*outputs[frame,:]-20*outputs_lin[frame,:])
-    var_linpf = np.var(20*outputs[frame,:]-20*outputs_linpf[frame,:])
-    print("frame: %d var_lin: %3.2f " % (frame,var_lin), end='')
-    if nn:
-        var_nnest = np.var(20*outputs[frame,:]-20*outputs_nnest[frame,:])
-        print("var_nnest: %3.2f" % (var_nnest), end='')
+                plt.plot(outputs_linpf[frame,st:st+eband_K],'r')
+        plt.ylim((0,10))
+    var_lin = np.var(10*outputs[frame,:]-10*outputs_lin[frame,:])
+    var_linpf = np.var(10*outputs[frame,:]-10*outputs_linpf[frame,:])
+    var_linstep = np.var(10*outputs[frame,:]-10*outputs_linstep[frame,:])
+    print("frame: %d var_lin (b): %3.2f " % (frame,var_lin), end='')
+    if args.nn:
+        var_nnest = np.var(10*outputs[frame,:]-10*outputs_nnest[frame,:])
+        print("var_nnest(r): %3.2f" % (var_nnest), end='')
+    elif args.nnpf:
+        var_nnpfest = np.var(10*outputs[frame,:]-10*outputs_linpf_est[frame,:])
+        print("var_nnpfest(r): %3.2f" % (var_nnpfest), end='')
     else:
-        print("var_linpf: %3.2f" % (var_linpf), end='')
+        print("var_linpf(r): %3.2f var_linstep(c): %3.2f" % (var_linpf, var_linstep), end='')
         
     print(flush=True)
     plt.show(block=False)
