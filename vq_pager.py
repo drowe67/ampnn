6e24168e6ce1a9a4ee4d9f9fbc9c9134e5e4a36f
David <david@rowetel.com>
2020-01-23 07:08:26 +1030

    feature to compare two VQ files

--- vq_pager.py
+++ vq_pager.py
@@ -16,31 +16,41 @@ import codec2_model
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Plot VQ entries')
-parser.add_argument('K', type=int, help='dimenion of eac vector')
-parser.add_argument('filename', help='.f32 vqfile')
+parser = argparse.ArgumentParser(description='Plot vector entries')
+parser.add_argument('K', type=int, help='dimenion of each vector')
+parser.add_argument('file1', help='.f32 vqfile')
+parser.add_argument('--file2', help='.f32 vqfile')
 args = parser.parse_args()
 
 K = args.K
 
 # read in rate K vectors
-vq = np.fromfile(args.filename, dtype='float32')
-M = int(len(vq)/K)
-print("VQ entries: %d" % (M))
-vq = np.reshape(vq, (M, K))
+vq = np.fromfile(args.file1, dtype='float32')
+M1 = int(len(vq)/K)
+print("file1 entries: %d" % (M1))
+vq1 = np.reshape(vq, (M1, K))
 
+if args.file2:
+    vq = np.fromfile(args.file2, dtype='float32')
+    M2 = int(len(vq)/K)
+    print("file2 entries: %d" % (M2))
+    vq2 = np.reshape(vq, (M2, K))
+    
 # plot results
 
 nb_plots = 16
 nb_plotsy = np.floor(np.sqrt(nb_plots)); nb_plotsx=nb_plots/nb_plotsy;
 offset = 0
 loop=True
-while loop and (offset < M):
+while loop and (offset < M1):
     plt.figure(1)
     for r in range(nb_plots):
         plt.subplot(nb_plotsy,nb_plotsx,r+1)
-        plt.plot(vq[offset+r,:])
-        plt.ylim(-20,20)
+        plt.plot(vq1[offset+r,:],'g')
+        if args.file2:
+            plt.plot(vq2[offset+r,:],'r')
+            
+        #plt.ylim(-20,20)
         plt.show(block=False)
 
     print("[%d .. %d] Any key for next page, click to finish...." % (offset, offset+nb_plots))
