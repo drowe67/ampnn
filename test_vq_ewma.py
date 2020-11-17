77238c5437e00e1603cf55f82bdf65404055f32a
David <david@rowetel.com>
2020-11-18 07:19:14 +1030

    rotated weights to be more sensible for my VQ work

--- test_vq_ewma.py
+++ test_vq_ewma.py
@@ -22,7 +22,7 @@ class VQVAELayer(tf.keras.layers.Layer):
 
     def build(self, input_shape):
         # Add embedding weights.
-        self.w = tf.Variable(initial_value=tf.transpose(tf.constant([[1.,1.],[-1.,1.],[-1.,-1.],[1.,-1.]])),
+        self.w = tf.Variable(initial_value=tf.constant([[1.,1.],[-1.,1.],[-1.,-1.],[1.,-1.]]),
                                trainable=False)
 
         # running sums/EWMA filter states
@@ -37,19 +37,20 @@ class VQVAELayer(tf.keras.layers.Layer):
         
         # Calculate distances of input to embedding vectors
         distances = (tf.math.reduce_sum(flat_inputs**2, axis=1, keepdims=True)
-                     - 2 * tf.tensordot(flat_inputs, self.w, 1)
-                     + tf.math.reduce_sum(self.w ** 2, axis=0, keepdims=True))
+                     - 2 * tf.tensordot(flat_inputs, tf.transpose(self.w), 1)
+                     + tf.math.reduce_sum(tf.transpose(self.w) ** 2, axis=0, keepdims=True))
         # Retrieve encoding indices
         encoding_indices = tf.argmax(-distances, axis=1)
         encoding_onehot = tf.one_hot(encoding_indices, self.num_embeddings)
-        quantized = tf.matmul(encoding_onehot,tf.transpose(self.w))
-
-        # Update VQ using EWMA 
-        centroid_sum =  tf.matmul(tf.transpose(x),encoding_onehot)
+        quantized = tf.matmul(encoding_onehot,self.w)
+        
+        # Update VQ using EWMA
+        centroid_sum =  tf.matmul(tf.transpose(encoding_onehot),x)
         centroid_n = tf.reduce_sum(encoding_onehot,axis=0)
         self.Centroid_sum = self.Centroid_sum*self.gamma + centroid_sum*(1-self.gamma)
         self.Centroid_n = self.Centroid_n*self.gamma + centroid_n*(1-self.gamma)
-        self.w = self.Centroid_sum/self.Centroid_n
+        print(self.Centroid_sum, self.Centroid_n, tf.reshape(self.Centroid_n, (-1, 1)))
+        self.w = self.Centroid_sum/tf.reshape(self.Centroid_n, (-1, 1))
         
         return quantized
 
@@ -58,7 +59,7 @@ class VQVAELayer(tf.keras.layers.Layer):
 
     def quantize(self, encoding_indices):
         encoding_onehot = tf.one_hot(encoding_indices, self.num_embeddings)
-        return = tf.matmul(encoding_onehot,tf.transpose(self.w))
+        return tf.matmul(encoding_onehot,self.w)
 
 print(tf.test.is_gpu_available())
 vq = VQVAELayer(embedding_dim=2,num_embeddings=4,commitment_cost=0.25)
