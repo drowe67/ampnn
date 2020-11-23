b626e38d74ae458abc5ced6a5f06843c1bd075ae
David <david@rowetel.com>
2020-11-24 06:55:19 +1030

    some refactoring

--- test_eager.py
+++ test_eager.py
@@ -2,60 +2,58 @@ import logging
 import os
 import numpy as np
 
+# Give TF "a bit of shoosh"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
 logging.getLogger('tensorflow').setLevel(logging.FATAL)
 import tensorflow as tf
 
-class MyDenseLayer(tf.keras.layers.Layer):
+# Custom Layer - Vector Quantiser Exponential Weighted Moving Average kmeans updates
+class VQ_EWMA_kmeans(tf.keras.layers.Layer):
     def __init__(self, embedding_dim, num_embeddings):
-        super(MyDenseLayer, self).__init__()
+        super(VQ_EWMA_kmeans, self).__init__()
         self.embedding_dim = embedding_dim
         self.num_embeddings = num_embeddings
         self.vq = tf.Variable(tf.constant([[1.,1.],[-1.,1.],[-1.,-1.],[1.,-1.]]),trainable=False)
         self.commitment_cost = 0.25
         self.gamma = 0.99
-        self.Centroid_sum = tf.Variable(tf.constant([[1.,1.],[-1.,1.],[-1.,-1.],[1.,-1.]]),trainable=False)
-        self.Centroid_n = tf.Variable(initial_value=tf.ones([self.num_embeddings]), trainable=False)
+
+        # moving averages used for kmeans update of VQ on each batch
+        self.ewma_centroid_sum = tf.Variable(self.vq,trainable=False)
+        self.ewma_centroid_n = tf.Variable(initial_value=tf.ones([self.num_embeddings]), trainable=False)
 
     def build(self, input_shape):
-        # running sums/EWMA filter states
-        super(MyDenseLayer, self).build(input_shape)
+        super(VQ_EWMA_kmeans, self).build(input_shape)
 
     def call(self, x):
         # Flatten input except for last dimension
         flat_inputs = tf.reshape(x, (-1, self.embedding_dim))
         
-        # Calculate distances of input to embedding vectors
+        # Calculate distances of input to each VQ entry
         distances = (tf.math.reduce_sum(flat_inputs**2, axis=1, keepdims=True)
                      - 2 * tf.tensordot(flat_inputs, tf.transpose(self.vq), 1)
                      + tf.math.reduce_sum(tf.transpose(self.vq) ** 2, axis=0, keepdims=True))
-        # Retrieve encoding indices
+        # Retrieve VQ indices
         encoding_indices = tf.argmax(-distances, axis=1)
         encoding_onehot = tf.one_hot(encoding_indices, self.num_embeddings)
         quantized = tf.matmul(encoding_onehot,self.vq)
         
-        # Update VQ using EWMA
+        # Update moving averages and hence vq 
         
         centroid_sum =  tf.matmul(tf.transpose(encoding_onehot),x)
         centroid_n = tf.reduce_sum(encoding_onehot,axis=0)
-        Centroid_sum = self.Centroid_sum*self.gamma + centroid_sum*(1.-self.gamma)
-        Centroid_n = self.Centroid_n*self.gamma + centroid_n*(1.-self.gamma)
-        vq = Centroid_sum/tf.reshape(Centroid_n, (-1, 1))
+        ewma_centroid_sum = self.ewma_centroid_sum*self.gamma + centroid_sum*(1.-self.gamma)
+        ewma_centroid_n = self.ewma_centroid_n*self.gamma + centroid_n*(1.-self.gamma)
+        vq = ewma_centroid_sum/tf.reshape(ewma_centroid_n, (-1, 1))
         
-        #tf.keras.backend.update(self.Centroid_sum, self.Centroid_sum*self.gamma + centroid_sum*(1.-self.gamma))
-        #tf.keras.backend.update(self.Centroid_sum, self.Centroid_sum*self.gamma)
-        #tf.keras.backend.update(self.Centroid_n, self.Centroid_n*self.gamma + centroid_n*(1.-self.gamma))
-        #tf.keras.backend.update(self.vq, self.Centroid_sum/tf.reshape(self.Centroid_n, (-1, 1)))
-        
-        tf.keras.backend.update(self.Centroid_sum, Centroid_sum)
-        tf.keras.backend.update(self.Centroid_n, Centroid_n)
+        tf.keras.backend.update(self.ewma_centroid_sum, ewma_centroid_sum)
+        tf.keras.backend.update(self.ewma_centroid_n, ewma_centroid_n)
         tf.keras.backend.update(self.vq, vq)
-        tf.print(Centroid_sum, Centroid_n, self.vq)
+        tf.print(ewma_centroid_sum, ewma_centroid_n, self.vq)
         
         return quantized
 
 inputs = tf.keras.layers.Input(shape=(2,))
-outputs = MyDenseLayer(2,4)(inputs)
+outputs = VQ_EWMA_kmeans(2,4)(inputs)
 
 model = tf.keras.Model(inputs, outputs)
 model.compile(loss='mse',optimizer='adam')
