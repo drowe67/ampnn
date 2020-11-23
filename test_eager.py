a1fe6bd5a4fae48e547ac3373f67b725a7891e92
David <david@rowetel.com>
2020-11-23 20:48:51 +1030

    found correct function for assignment I think, still a WIP

--- test_eager.py
+++ test_eager.py
@@ -14,8 +14,11 @@ class MyDenseLayer(tf.keras.layers.Layer):
         self.vq = tf.Variable(tf.constant([[1.,1.],[-1.,1.],[-1.,-1.],[1.,-1.]]),trainable=False)
         self.commitment_cost = 0.25
         self.gamma = 0.99
+        self.Centroid_sum = self.vq
+        self.Centroid_n = tf.Variable(initial_value=tf.ones([self.num_embeddings]), trainable=False)
 
     def build(self, input_shape):
+        # running sums/EWMA filter states
         super(MyDenseLayer, self).build(input_shape)
 
     def call(self, x):
@@ -31,6 +34,14 @@ class MyDenseLayer(tf.keras.layers.Layer):
         encoding_onehot = tf.one_hot(encoding_indices, self.num_embeddings)
         quantized = tf.matmul(encoding_onehot,self.vq)
         
+        # Update VQ using EWMA
+        
+        centroid_sum =  tf.matmul(tf.transpose(encoding_onehot),x)
+        centroid_n = tf.reduce_sum(encoding_onehot,axis=0)
+        tf.keras.backend.update(self.Centroid_sum, self.Centroid_sum*self.gamma + centroid_sum*(1-self.gamma))
+        tf.keras.backend.update(self.Centroid_n, self.Centroid_n*self.gamma + centroid_n*(1-self.gamma))
+        tf.keras.backend.update(self.vq, self.Centroid_sum/tf.reshape(self.Centroid_n, (-1, 1)))
+        
         return quantized
 
 inputs = tf.keras.layers.Input(shape=(2,))
@@ -39,6 +50,8 @@ outputs = MyDenseLayer(2,4)(inputs)
 model = tf.keras.Model(inputs, outputs)
 model.compile(loss='mse',optimizer='adam')
 model.summary()
+
 x_train=np.ones((1000,2),dtype=float);
+
 model.fit(x_train, x_train, batch_size=1, epochs=2)
 
