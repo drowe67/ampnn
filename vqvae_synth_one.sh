90d6a49e9d9e9634ab7960a2819215ba0c4193ea
David <david@rowetel.com>
2020-12-29 13:15:44 +1030

    phase0 output file

--- vqvae_synth_one.sh
+++ vqvae_synth_one.sh
@@ -19,4 +19,4 @@ tmp_dir=$(mktemp -d)
 c2sim $1 --rateK --rateKout ${tmp_dir}/${s}.f32
 ./vq_vae_kmeans_conv1d_out.py ${npy} ${tmp_dir}/${s}.f32 --featurefile_out ${tmp_dir}/${s}_out.f32 --eband_K 20 --scale 0.005 --mean
 c2sim $1  --rateK --rateKin ${tmp_dir}/${s}_out.f32 -o ${out_dir}/${s}_out.raw
-c2enc 700C $1 - | c2dec 700C - ${out_dir}/${s}_700c.raw
+c2sim $1  --rateK --rateKin ${tmp_dir}/${s}_out.f32 --phase0 --postfilter -o ${out_dir}/${s}_outp0.raw
