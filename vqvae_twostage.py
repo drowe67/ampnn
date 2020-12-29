6e696153067e33dae6322ac500f5a2e94405bc89
David <david@rowetel.com>
2020-12-29 17:42:39 +1030

    trying striedes=2 rather than maxpooling

--- vqvae_twostage.py
+++ vqvae_twostage.py
@@ -11,9 +11,8 @@ def vqvae_models(nb_timesteps, nb_features, dim, num_embedding):
     x = tf.keras.layers.Input(shape=(nb_timesteps+2, nb_features), name='encoder_input')
 
     # Encoder
-    z_e = tf.keras.layers.Conv1D(32, 3, activation='tanh', padding='valid', name="conv1d_a")(x)
-    z_e = tf.keras.layers.MaxPooling1D(pool_size=2, padding='same')(z_e)
-    z_e = tf.keras.layers.Conv1D(dim, 3, activation='tanh', padding='same', name="conv1d_b")(z_e)
+    z_e = tf.keras.layers.Conv1D(32, 3, activation='tanh', padding='valid', strides=2, name="conv1d_a")(x)
+    z_e = tf.keras.layers.Conv1D(dim, 3, activation='tanh', padding='same', strides=1, name="conv1d_b")(z_e)
 
     # VQ
     z_q1 = VQ_kmeans(dim, num_embedding, name="vq1")(z_e)
